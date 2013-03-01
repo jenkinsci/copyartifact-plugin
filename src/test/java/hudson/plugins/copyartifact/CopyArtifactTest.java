jenkinsci/copyartifact-plugin

commit 6243f6bcd5ebe872d3c03c8d740b45e3d714f1c4

Splitting projectName field into project and parameters fields for clarity and robustness.

src/main/java/hudson/plugins/copyartifact/CopyArtifact.java
@@ -83,14 +83,16 @@ import org.kohsuke.stapler.StaplerRequest;
  */
 public class CopyArtifact extends Builder {
 
-    private String projectName;
+    @Deprecated private transient String projectName;
+    private String project;
+    private String parameters;
     private final String filter, target;
     private /*almost final*/ BuildSelector selector;
     @Deprecated private transient Boolean stable;
     private final Boolean flatten, optional;
 
     @DataBoundConstructor
-    public CopyArtifact(String projectName, BuildSelector selector, String filter, String target,
+    public CopyArtifact(String projectName, String parameters, BuildSelector selector, String filter, String target,
                         boolean flatten, boolean optional) {
         // check the permissions only if we can
         StaplerRequest req = Stapler.getCurrentRequest();
@@ -100,11 +102,12 @@ public class CopyArtifact extends Builder {
 
             // Prevents both invalid values and access to artifacts of projects which this user cannot see.
             // If value is parameterized, it will be checked when build runs.
-            if (projectName.indexOf('$') < 0 && new JobResolver(context, projectName).job == null)
+            if (projectName.indexOf('$') < 0 && Jenkins.getInstance().getItem(projectName, context, Job.class) == null)
                 projectName = ""; // Ignore/clear bad value to avoid ugly 500 page
         }
 
-        this.projectName = projectName;
+        this.project = projectName;
+        this.parameters = Util.fixEmptyAndTrim(parameters);
         this.selector = selector;
         this.filter = Util.fixNull(filter).trim();
         this.target = Util.fixNull(target).trim();
@@ -120,11 +123,26 @@ public class CopyArtifact extends Builder {
                 obj.selector = new StatusBuildSelector(obj.stable != null && obj.stable);
                 OldDataMonitor.report(context, "1.355"); // Core version# when CopyArtifact 1.2 released
             }
+            if (obj.projectName != null) {
+                int i = obj.projectName.lastIndexOf('/');
+                if (i != -1 && obj.projectName.indexOf('=', i) != -1) {
+                    obj.project = obj.projectName.substring(0, i);
+                    obj.parameters = obj.projectName.substring(i + 1);
+                } else {
+                    obj.project = obj.projectName;
+                    obj.parameters = null;
+                }
+                obj.projectName = null;
+            }
         }
     }
 
     public String getProjectName() {
-        return projectName;
+        return project;
+    }
+    
+    public String getParameters() {
+        return parameters;
     }
 
     public BuildSelector getBuildSelector() {
@@ -151,28 +169,28 @@ public class CopyArtifact extends Builder {
     public boolean perform(AbstractBuild<?,?> build, Launcher launcher, BuildListener listener)
             throws InterruptedException {
         PrintStream console = listener.getLogger();
-        String expandedProject = projectName, expandedFilter = filter;
+        String expandedProject = project, expandedFilter = filter;
         try {
             EnvVars env = build.getEnvironment(listener);
             env.overrideAll(build.getBuildVariables()); // Add in matrix axes..
-            expandedProject = env.expand(projectName);
-            JobResolver job = new JobResolver(build.getProject().getParent(),expandedProject);
-            if (job.job != null && !expandedProject.equals(projectName)
+            expandedProject = env.expand(project);
+            Job job = Jenkins.getInstance().getItem(expandedProject, build.getProject().getParent(), Job.class);
+            if (job != null && !expandedProject.equals(project)
                 // If projectName is parameterized, need to do permission check on source project.
                 // Would like to check if user who started build has permission, but unable to get
                 // Authentication object for arbitrary user.. instead, only allow use of parameters
                 // to select jobs which are accessible to all authenticated users.
-                && !job.job.getACL().hasPermission(
+                && !job.getACL().hasPermission(
                         new UsernamePasswordAuthenticationToken("authenticated", "",
                                 new GrantedAuthority[]{ SecurityRealm.AUTHENTICATED_AUTHORITY }),
                         Item.READ)) {
-                job.job = null; // Disallow access
+                job = null; // Disallow access
             }
-            if (job.job == null) {
+            if (job == null) {
                 console.println(Messages.CopyArtifact_MissingProject(expandedProject));
                 return false;
             }
-            Run src = selector.getBuild(job.job, env, job.filter, build);
+            Run src = selector.getBuild(job, env, parameters != null ? new ParametersBuildFilter(parameters) : new BuildFilter(), build);
             if (src == null) {
                 console.println(Messages.CopyArtifact_MissingBuild(expandedProject));
                 return isOptional();  // Fail build unless copy is optional
@@ -252,31 +270,6 @@ public class CopyArtifact extends Builder {
         }
     }
 
-    // Find the job from the given name; usually just a Hudson.getItemByFullName lookup,
-    // but this class encapsulates additional logic like filtering on parameters.
-    private static class JobResolver {
-        Job<?,?> job;
-        BuildFilter filter = new BuildFilter();
-
-        JobResolver(ItemGroup context, String projectName) {
-            job = Jenkins.getInstance().getItem(projectName, context, Job.class);
-            if (job == null) {
-                // Check for parameterized job with filter (see help file)
-                int i = projectName.lastIndexOf('/');
-                if (i > 0) {
-                    Job<?,?> candidate = Jenkins.getInstance().getItem(projectName.substring(0, i), context, Job.class);
-                    if (candidate != null) {
-                        ParametersBuildFilter pFilter = new ParametersBuildFilter(projectName.substring(i + 1));
-                        if (pFilter.isValid(candidate)) {
-                            job = candidate;
-                            filter = pFilter;
-                        }
-                    }
-                }
-            }
-        }
-    }
-
     @Extension
     public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
 
@@ -285,7 +278,7 @@ public class CopyArtifact extends Builder {
             // Require CONFIGURE permission on this project
             if (!anc.hasPermission(Item.CONFIGURE)) return FormValidation.ok();
             FormValidation result;
-            Item item = new JobResolver(anc.getParent(),value).job;
+            Item item = Jenkins.getInstance().getItem(value, anc.getParent());
             if (item != null)
                 result = item instanceof MavenModuleSet
                        ? FormValidation.warning(Messages.CopyArtifact_MavenProject())
@@ -323,10 +316,10 @@ public class CopyArtifact extends Builder {
                     : Hudson.getInstance().getAllItems(AbstractProject.class)) {
                 for (CopyArtifact ca : getCopiers(project)) try {
                     if (ca.getProjectName().equals(oldName))
-                        ca.projectName = newName;
+                        ca.project = newName;
                     else if (ca.getProjectName().startsWith(oldName + '/'))
                         // Support rename for "MatrixJobName/AxisName=value" type of name
-                        ca.projectName = newName + ca.projectName.substring(oldName.length());
+                        ca.project = newName + ca.project.substring(oldName.length());
                     else continue;
                     project.save();
                 } catch (IOException ex) {

src/main/resources/hudson/plugins/copyartifact/CopyArtifact/config.jelly
@@ -34,6 +34,9 @@ THE SOFTWARE.
   <f:entry title="${%Target directory}" field="target">
     <f:textbox/>
   </f:entry>
+  <f:entry title="${%Parameter filters}" field="parameters">
+    <f:textbox/>
+  </f:entry>
   <f:entry help="/plugin/copyartifact/help-flatten-optional.html">
     <f:checkbox field="flatten"/>
     <label class="attach-previous">${%Flatten directories}</label>

src/main/resources/hudson/plugins/copyartifact/CopyArtifact/help-parameters.html
@@ -1,0 +1,10 @@
+<div>
+  Jobs may be filtered to select only builds matching particular parameters or
+  other build variables. Use <tt>PARAM=VALUE,...</tt>
+  to list the parameter filter; this is the same syntax as described for
+  multiconfiguration jobs in <i>Project name</i>
+  except with parameters instead of axis values.
+  For example, <tt>FOO=bar,BAZ=true</tt> examines only builds
+  that ran with parameter <tt>FOO</tt> set to <tt>bar</tt>
+  and the checkbox for <tt>BAZ</tt> was checked.
+</div>

src/main/resources/hudson/plugins/copyartifact/CopyArtifact/help-projectName.html
@@ -21,12 +21,4 @@
   <br/>  To copy artifacts from one matrix project to another, use a parameter to
   select the matching configuration in the source project.
   Example: <tt>OtherMatrixJob/jdk=$jdk</tt>
-  <p/>
-  Jobs may be filtered to select only builds matching particular parameters or
-  other build variables. Use <tt>JOBNAME/PARAM=VALUE,...</tt> in the project name
-  box to list the parameter filter; this is the same syntax as described above for
-  multiconfiguration jobs, except with parameters instead of axis values.
-  For example, <tt>MyParamJob/FOO=bar,BAZ=true</tt> examines only builds from
-  <tt>MyParamJob</tt> that ran with parameter <tt>FOO</tt> set to <tt>bar</tt>
-  and the checkbox for <tt>BAZ</tt> was checked.
 </div>

src/main/resources/hudson/plugins/copyartifact/CopyArtifact/help-projectName_de.html
@@ -22,11 +22,4 @@
   ein Parameter benutzt werden um die richtige Konfiguration im Quellprojekt auszuw&auml;hlen.
   Beispiel: <tt>OtherMatrixJob/jdk=$jdk</tt>
   </p>
-  <p>Jobs k&ouml;nnen gefiltert werden, damit nur Builds, die bestimmten Parametern oder anderen Buildvariablen
-  entsprechen, ausgew&auml;hlt werden. <tt>JOBNAME/PARAM=VALUE,...</tt> kann im Projektnamen-Feld benutzt werden
-  um Parameter-Filter aufzulisten; das entspricht der selben Syntax wie f&uuml;r Matrixjobs, nur mit
-  Parameter- statt Achsenwerten.<br/>
-  Beispiel: <tt>MyParamJob/FOO=bar,BAZ=true</tt> ber&uuml;cksichtigt nur Builds
-  von <tt>MyParamJob</tt>, bei denen der Parameter <tt>FOO</tt> auf <tt>bar</tt> gesetzt und die Checkbox
-  <tt>BAZ</tt> selektiert wurde.</p>
 </div>

src/main/resources/hudson/plugins/copyartifact/CopyArtifact/help-projectName_ja.html
@@ -18,9 +18,4 @@
   マルチ構成プロジェクトから他のマルチ構成プロジェクトにコピーするには、コピー元のプロジェクトでの条件としてパラメータを設定します。<br/>
   例: <tt>OtherMatrixJob/jdk=$jdk</tt>
   </p>
-  <p>
-  パラメータや変数を追加することでジョブを特定することができます。パラメータで特定するには、プロジェクト名に<tt>JOBNAME/PARAM=VALUE,...</tt>を設定します。
-  マルチ構成プロジェクトと同様の記述をしますが、軸のかわりにパラメーターを使用します。<br/>
-  例: <tt>MyParamJob/FOO=bar,BAZ=true</tt>は、<tt>MyParamJob</tt>プロジェクトで、<tt>FOO</tt>が<tt>bar</tt>で<tt>BAZ</tt>がチェックされたビルドを指定します。
-  </p>
 </div>

commit 78fd99e73f663ab2eabf8085c97fd8b6b1abcdb0

Restoring deserialization compatibility broken by 6243f6b for matrix projects. Needed to check whether projectName was in fact a real job name (MatrixRun). Unfortunately cannot use Converter.unmarshal for this purpose: - relative paths would not be supported, since there is no context - unpredictable order of job load breaks logic in case copier is loaded before matrix Instead, upgrading format if and when the project is run, when all information is available. (Note that this means that if you open the configure page before running it after upgrade, you will see the related fields as blank, so you would need to correct them first; not sure if that is fixable, but this is anyway a one-time conversion.)

src/main/java/hudson/plugins/copyartifact/CopyArtifact.java
@@ -83,7 +83,7 @@ import org.kohsuke.stapler.StaplerRequest;
  */
 public class CopyArtifact extends Builder {
 
-    @Deprecated private transient String projectName;
+    @Deprecated private String projectName;
     private String project;
     private String parameters;
     private final String filter, target;
@@ -123,17 +123,6 @@ public class CopyArtifact extends Builder {
                 obj.selector = new StatusBuildSelector(obj.stable != null && obj.stable);
                 OldDataMonitor.report(context, "1.355"); // Core version# when CopyArtifact 1.2 released
             }
-            if (obj.projectName != null) {
-                int i = obj.projectName.lastIndexOf('/');
-                if (i != -1 && obj.projectName.indexOf('=', i) != -1) {
-                    obj.project = obj.projectName.substring(0, i);
-                    obj.parameters = obj.projectName.substring(i + 1);
-                } else {
-                    obj.project = obj.projectName;
-                    obj.parameters = null;
-                }
-                obj.projectName = null;
-            }
         }
     }
 
@@ -167,7 +156,20 @@ public class CopyArtifact extends Builder {
 
     @Override
     public boolean perform(AbstractBuild<?,?> build, Launcher launcher, BuildListener listener)
-            throws InterruptedException {
+            throws InterruptedException, IOException {
+        if (projectName != null) {
+            int i = projectName.lastIndexOf('/');
+            if (i != -1 && projectName.indexOf('=', i) != -1 && /* not matrix */Jenkins.getInstance().getItem(projectName, build.getProject().getParent(), Job.class) == null) {
+                project = projectName.substring(0, i);
+                parameters = projectName.substring(i + 1);
+            } else {
+                project = projectName;
+                parameters = null;
+            }
+            Logger.getLogger(CopyArtifact.class.getName()).log(Level.INFO, "Split {0} into {1} with parameters {2}", new Object[] {projectName, project, parameters});
+            projectName = null;
+            build.getParent().save();
+        }
         PrintStream console = listener.getLogger();
         String expandedProject = project, expandedFilter = filter;
         try {

src/test/java/hudson/plugins/copyartifact/CopyArtifactTest.java
@@ -62,6 +62,7 @@ import hudson.util.VersionNumber;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.concurrent.Callable;
+import jenkins.model.Jenkins;
 
 import org.acegisecurity.context.SecurityContextHolder;
 import org.acegisecurity.providers.UsernamePasswordAuthenticationToken;
@@ -868,4 +869,26 @@ public class CopyArtifactTest extends HudsonTestCase {
         assertTrue(descriptor.getDisplayName().length() > 0);
         assertTrue(descriptor.getBuildSelectors().size() > 0);
     }
+
+    @LocalData
+    public void testProjectNameSplit() throws Exception {
+        FreeStyleProject parameterized = Jenkins.getInstance().getItemByFullName("parameterized", FreeStyleProject.class);
+        assertNotNull(parameterized);
+        assertTrue(((BooleanParameterValue) parameterized.getBuildByNumber(2).getAction(ParametersAction.class).getParameter("good")).value);
+        FreeStyleProject copier = Jenkins.getInstance().getItemByFullName("copier", FreeStyleProject.class);
+        assertNotNull(copier);
+        String configXml = copier.getConfigFile().asString();
+        assertTrue(configXml, configXml.contains("<projectName>matrix/which=two</projectName>"));
+        FreeStyleBuild build = copier.scheduleBuild2(0).get();
+        @SuppressWarnings("deprecation") String log = build.getLog();
+        assertEquals(log, Result.SUCCESS, build.getResult());
+        assertTrue(log, log.contains("OK"));
+        configXml = copier.getConfigFile().asString();
+        assertFalse(configXml, configXml.contains("<projectName>"));
+        assertTrue(configXml, configXml.contains("<project>plain</project>"));
+        assertTrue(configXml, configXml.contains("<project>parameterized</project>"));
+        assertTrue(configXml, configXml.contains("<parameters>good=true</parameters>"));
+        assertTrue(configXml, configXml.contains("<project>matrix/which=two</project>"));
+    }
+
 }