946f9e47059cede6a6b50934f86efca42b574e05
ikedam <devld@ikedam.jp>
2017-09-13 07:37:03 +0900

    Use Initializer instead of Plugin#postInitialize
    * Somehow `Jenkins#getDescriptorByType` fails in some environments called via `Plugin#postInitizalize`

--- src/main/java/hudson/plugins/copyartifact/BuildSelectorParameter.java
+++ src/main/java/hudson/plugins/copyartifact/BuildSelectorParameter.java
@@ -31,6 +31,8 @@ import com.thoughtworks.xstream.XStreamException;
 import jenkins.model.Jenkins;
 import hudson.DescriptorExtensionList;
 import hudson.Extension;
+import hudson.init.InitMilestone;
+import hudson.init.Initializer;
 import hudson.model.Descriptor;
 import hudson.model.Hudson;
 import hudson.model.ParameterValue;
@@ -146,7 +148,8 @@ public class BuildSelectorParameter extends SimpleParameterDefinition {
 
     private static final XStream2 XSTREAM = new XStream2();
 
-    static void initAliases() {
+    @Initializer(after=InitMilestone.PLUGINS_STARTED)
+    public static void initAliases() {
         Jenkins jenkins = Jenkins.getInstance();
         if (jenkins == null) {
             LOGGER.severe("Called for initialization but Jenkins instance no longer available.");
